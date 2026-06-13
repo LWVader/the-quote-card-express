d20a8dbf0ac64bbc79b1a173912c772456ef157c
L W <phantasmocazdor@gmail.com>
2026-06-13 19:08:16 -0400

    updated

--- public/js/index.js
+++ public/js/index.js
@@ -5,15 +5,14 @@ const elements = {
     author: document.getElementById("author"),
 };
 async function getRandomImage() {
-    const client_id = process.env.client_id;
-    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
-   try {
+    const endpoint = "http://localhost:8080/api/v1/getRandomImage";
+    try {
         const response = await fetch(endpoint);
         const returnedData = await response.json();
-        const receivedPhotoUrl = returnedData.urls.regular;
+        const receivedPhotoUrl = returnedData.data;
  
         const imgDiv = document.querySelector(".background-img");
-        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+        imgDiv.style.backgroundImage = `url(${receivedPhotoUrl})`;
     } catch (error) {
         console.error(error);
     }
